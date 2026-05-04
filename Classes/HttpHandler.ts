34e165176758cbddab3e1e9d63a122c7d63e0012
FtRookie <email@ftrookie.com>
2026-05-04 03:51:47 -0700

    make code look nicer add new endpoint correct format
    Co-authored-by: samlovebutter <samlovebutter@users.noreply.github.com>

--- Classes/HttpHandler.ts
+++ Classes/HttpHandler.ts
@@ -1,50 +1,53 @@
-
 import { Elysia, t } from 'elysia';
 import { DatabaseInteractions } from './DatabaseInteractions';
 import { Database } from "bun:sqlite";
 import { write_token } from '../Access Tokens/securityTokens';
 
-export namespace HttpHandler
-{
-    export const init = (db: Database, base: string, port: number) =>
-    {
+export namespace HttpHandler {
+    export const init = (db: Database, base: string, port: number) => {
         const app = new Elysia();
         app.listen(port);
 
-        app.get(`/${base}/player/:id`, ({ params: { id } }) =>
-        {
+        // read player data by id
+        app.get(`/${base}/player/:id`, ({ params: { id } }) => {
             const player = DatabaseInteractions.getPlayerDataEntryByID(db, id);
             return player ?? { error: 'Not found' };
         });
 
-        app.get(`/${base}/save/:id`, ({ params: { id } }) =>
-        {
-            const player = DatabaseInteractions.getSavesOfPlayerByID(db, id);
-            return player ?? { error: 'Not found' };
+        // read all saves by player id
+        app.get(`/${base}/save/:id`, ({ params: { id } }) => {
+            const saves = DatabaseInteractions.getSavesOfPlayerByID(db, id);
+            return saves ? ({ saves }) : { error: 'Not found' };
         });
 
-        app.post(`/${base}/player`, ({ body }) =>
-        {
-            DatabaseInteractions.insertPlayers(db, [body]);
+        // read single save by player id
+        app.get(`/${base}/save/:id/:index`, ({ params: { id, index } }) => {
+            const save = DatabaseInteractions.getSavesOfPlayerByIDWithIndex(db, id, index);
+            return save ?? { error: 'Not found' };
+        });
+
+        // write player
+        app.post(`/${base}/player`, ({ body }) => {
             if (body.token !== write_token) return { error: "incorrect token" };
+            DatabaseInteractions.insertPlayers(db, [body]);
             return { status: 'ok' };
         }, {
             body: t.Object({
-                slotIndex: t.Integer(),
-                playerId: t.String(),
+                playerID: t.String(),
                 data: t.String(),
                 token: t.String(),
             })
         });
 
-        app.post(`/${base}/save`, ({ body }) =>
-        {
-            DatabaseInteractions.insertSave(db, [body]);
+        // write save (I'm not doing batches)
+        app.post(`/${base}/save`, ({ body }) => {
             if (body.token !== write_token) return { error: "incorrect token" };
+            DatabaseInteractions.insertSave(db, [body]);
             return { status: 'ok' };
         }, {
             body: t.Object({
-                playerId: t.String(),
+                playerID: t.String(),
+                index: t.String(),
                 data: t.String(),
                 token: t.String(),
             })
