e1b05011296d110fc43be2255b9e7961c439bb3c
samlovebutter <dima_eremen@mail.ru>
2026-06-06 22:14:06 +0300

    fix tests

--- testing/tests.ts
+++ testing/tests.ts
@@ -2,8 +2,10 @@ import { beforeEach, afterEach, test } from "bun:test";
 import { WRITE_TOKEN } from "..";
 import type { ErrorType } from "../Classes/HttpHandler";
 
-const playerID = "238427763";
+const playerID = "1";
 const slotIndex = "31";
+
+//todo add rename + remove + run
 
 // run with 
 // bun test .\testing\tests.ts
@@ -21,23 +23,27 @@ afterEach(() =>
 test("retrieve player metadata", async () =>
 {
     const d = await fetch(`http://localhost:1367/overengineered/player/${playerID}`);
-    await d.json();
+    const parsedData = await d.json();
+    if (parsedData.error) throw `Unable to reach example save data: ${parsedData}`;
+    console.log("Player meta retrieved and parsed:", parsedData);
 });
 
 test("retrieve all player saves", async () =>
 {
     const d = await fetch(`http://localhost:1367/overengineered/save/${playerID}`);
     const parsedData = await d.json();
-    if (parsedData.error) throw "Unable to reach example save data"
-    console.log("All saves retrieved and parsed");
+    if (parsedData.error) throw `Unable to reach example save data: ${parsedData}`;
+    if (!parsedData.saves) throw `Expected save data, got ${JSON.stringify(parsedData).slice(0, 50)}...`;
+    console.log("All saves retrieved and parsed:", parsedData);
 });
 
 test("retrieve single player save", async () =>
 {
     const d = await fetch(`http://localhost:1367/overengineered/save/${playerID}/${slotIndex}`);
     const parsedData = await d.json();
-    if (parsedData.error) throw "Unable to reach example save data"
-    console.log("Single save retrieved and parsed");
+    if (parsedData.error) throw `Unable to reach example save data: ${parsedData}`;
+    if (!parsedData.blocks) throw `Expected save data, got ${JSON.stringify(parsedData).slice(0, 50)}...`;
+    console.log("All saves retrieved and parsed:", parsedData);
 });
 
 
@@ -51,7 +57,7 @@ test("retrieve single player save page by page", async () =>
         const text = await d.text();
         try {
             const j = JSON.parse(text) as { error: string, err_type: ErrorType };
-            if (j.err_type) {
+            if (j.error) {
                 if (j.err_type === "OUT_OF_INDEX") break;
                 err = `Got unexpected error: ${j}`;
                 break;
@@ -67,7 +73,8 @@ test("retrieve single player save page by page", async () =>
 
     // parse result
     res ??= JSON.parse(cachedData.join(""));
-    console.log("Single save retrieved page by page and parsed");
+    if (!res.data) throw `Expected save data, got ${JSON.stringify(res).slice(0, 50)}...`;
+    console.log("Single save retrieved page by page and parsed:", res);
 });
 
 
@@ -83,29 +90,30 @@ test("Write save data with token", async () =>
         return d.json();
     }
 
-    const before = await getData();
-    before.testString ??= "a";
-    before.testString += "b";
-    if (before.testString.length > 100) before.testString = "a";
-
-    const data: {}[] = before;
-    const writeResult = await fetch(
+    const writeData = (wd: { [key: string]: unknown }) => fetch(
         `http://localhost:1367/overengineered/save`,
         {
             method: "POST",
             body: JSON.stringify({
                 playerID,
                 index: slotIndex,
-                data,
+                data: wd,
                 token: WRITE_TOKEN,
             }),
             headers
         });
 
+    // begins here
+    const before = await getData();
+
+    const writeResult = await writeData(before);
     if (!writeResult.ok) throw "Incorrect data format";
 
     const after = await getData();
-    if (JSON.stringify(before) === JSON.stringify(after)) throw "Written data is the same as read data."
+    // console.log(before);
+    console.log(after);
+    if (JSON.stringify(before) !== JSON.stringify(after)) throw "Written data is not the same as read data."
 
+    // await writeData(before);
     console.log("Single save retrieved page by page and parsed");
 });
